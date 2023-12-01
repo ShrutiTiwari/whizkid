56ae1badc398efc136950015678c691413e88c91
ShrutiTiwari <reachstiwari@gmail.com>
2023-12-01 13:03:08 +0000

    first commit- static resource

---
+++ src/CarThemedImageDB.js
@@ -0,0 +1,247 @@
+const Alphabets = [
+    {
+      id: '1',
+      name: 'A',    
+      type: "b",
+      src: require('../assets/carthemed/ambulance.jpg'),
+      sound: require('../assets/carthemed/sound/a_ambulance.m4a'),
+      objectName: 'Ambulance',
+    },
+    {
+      id: '2',
+      name: 'B',
+      type: "b",
+      src: require('../assets/carthemed/bus.jpg'),
+      sound: require('../assets/carthemed/sound/b_bus.m4a'),
+      objectName: 'Bus',
+    },
+    {
+      id: '3',
+      name: 'C',
+      type: "b",
+      src: require('../assets/carthemed/car.jpg'),
+      sound: require('../assets/carthemed/sound/c_car.m4a'),
+      objectName: 'Car',
+    },
+    {
+      id: '4',
+      name: 'D',
+      type: "b",
+      src: require('../assets/carthemed/driver.jpg'),
+      sound: require('../assets/carthemed/sound/d_driver.m4a'),
+      objectName: 'Driver',
+    },
+    {
+      id: '5',
+      name: 'E',
+      type: "b",
+      src: require('../assets/carthemed/escalator.jpg'),
+      sound: require('../assets/carthemed/sound/e_escalator.m4a'),
+      objectName: 'Escalator',
+    },
+    {
+      id: '6',
+      name: 'F',
+      type: "b",
+      src: require('../assets/carthemed/firetruck.jpg'),
+      sound: require('../assets/carthemed/sound/f_firetruck.m4a'),
+      objectName: 'Fire Truck',
+    },
+    
+    {
+      id: '101',
+      name: 'WellDoneMessage',
+      type: "b",
+      src: require('../assets/carthemed/screen/yay.jpg'),
+      objectName: 'Well Done ! ',
+    },
+    {
+      id: '7',
+      name: 'G',
+      type: "b",
+      src: require('../assets/carthemed/garbage_truck.jpg'),
+      sound: require('../assets/carthemed/sound/g_garbage_truck.m4a'),
+      objectName: 'Garbage Truck',
+    },
+  
+    {
+      id: '8',
+      name: 'H',
+      type: "b",
+      src: require('../assets/carthemed/helicopter.jpg'),
+      sound: require('../assets/carthemed/sound/h_helicopter.m4a'),
+      objectName: 'Helicopter',
+    },
+    
+    {
+      id: '9',
+      name: 'I',
+      type: "b",
+      src: require('../assets/carthemed/icecream_truck.jpg'),
+      sound: require('../assets/carthemed/sound/i_icecream_truck.m4a'),
+      objectName: 'Ice Cream Truck',
+    },
+  
+    
+    {
+      id: '10',
+      name: 'J',
+      type: "b",
+      src: require('../assets/carthemed/jeep.jpg'),
+      sound: require('../assets/carthemed/sound/j_jeep.m4a'),
+      objectName: 'Jeep',
+    },
+    {
+      id: '11',
+      name: 'K',
+      type: "b",
+      src: require('../assets/carthemed/keys.jpg'),
+      sound: require('../assets/carthemed/sound/k_keys.m4a'),
+      objectName: 'Keys',
+    },
+    
+    {
+      id: '12',
+      name: 'L',
+      type: "b",
+      src: require('../assets/carthemed/lorry.jpg'),
+      sound: require('../assets/carthemed/sound/l_lorry.m4a'),
+      objectName: 'Lorry',
+    },
+    {
+      id: '102',
+      name: 'WellDoneMessage',
+      type: "b",
+      src: require('../assets/carthemed/screen/yay.jpg'),
+      objectName: 'Great Job! Half way through.',
+    },  
+    {
+      id: '13',
+      name: 'M',
+      type: "b",
+      src: require('../assets/carthemed/motorcycle.jpg'),
+      sound: require('../assets/carthemed/sound/m_motorcycle.m4a'),
+      objectName: 'Motorcycle',
+    },
+    
+    {
+      id: '14',
+      name: 'N',
+      type: "b",
+      src: require('../assets/carthemed/number_plate.jpg'),
+      sound: require('../assets/carthemed/sound/n_number_plate.m4a'),
+      objectName: 'Number Plate',
+    },
+    
+    {
+      id: '15',
+      name: 'O',
+      type: "b",
+      src: require('../assets/carthemed/odometer.jpg'),
+      sound: require('../assets/carthemed/sound/o_odometer.m4a'),
+      objectName: 'Odometer',
+    },
+    
+    {
+      id: '16',
+      name: 'P',
+      type: "b",
+      src: require('../assets/carthemed/police_car.jpg'),
+      sound: require('../assets/carthemed/sound/p_police_car.m4a'),
+      objectName: 'Police Car',
+    },
+    
+    {
+      id: '17',
+      name: 'Q',
+      type: "b",
+      src: require('../assets/carthemed/quad_bike.jpg'),
+      sound: require('../assets/carthemed/sound/q_quad_bike.m4a'),
+      objectName: 'Quad Bike',
+    },
+    {
+      id: '18',
+      name: 'R',
+      type: "b",
+      src: require('../assets/carthemed/racing_car.jpg'),  
+      sound: require('../assets/carthemed/sound/r_racecar.m4a'),  
+      objectName: 'Race Car',
+    },
+    
+    {
+      id: '103',
+      name: 'WellDoneMessage',
+      type: "b",
+      src: require('../assets/carthemed/screen/yay.jpg'),    
+      objectName: 'You are doing amazing! ',
+    },
+    {
+      id: '19',
+      name: 'S',
+      type: "b",
+      src: require('../assets/carthemed/submarine.jpg'),    
+      objectName: 'Submarine',
+    },
+  
+    {
+      id: '20',
+      name: 'T',
+      type: "b",
+      src: require('../assets/carthemed/taxi.jpg'),
+      sound: require('../assets/carthemed/sound/t_taxi.m4a'),
+      objectName: 'Taxi',
+    },
+  
+    
+    {
+      id: '21',
+      name: 'U',
+      type: "o",
+      src: require('../assets/carthemed/unlock.jpg'),
+      sound: require('../assets/carthemed/sound/u_unlock.m4a'),
+      objectName: 'Unlock',
+    },
+    
+    {
+      id: '22',
+      name: 'V',
+      type: "b",
+      src: require('../assets/carthemed/van.jpg'),
+      sound: require('../assets/carthemed/sound/v_van.m4a'),
+      objectName: 'Van',
+    },
+    
+    {
+      id: '23',
+      name: 'W',
+      type: "b",
+      src: require('../assets/carthemed/wheel.jpg'),
+      objectName: 'Wheel',
+    },
+    
+    {
+      id: '24',
+      name: 'X',
+      type: "b",
+      src: require('../assets/carthemed/xray_truck.jpg'),
+      objectName: 'X-Ray Truck',
+    },
+    
+    {
+      id: '25',
+      name: 'Y',
+      type: "b",
+      src: require('../assets/carthemed/yacht.jpg'),
+      objectName: 'Yacht',
+    },
+    
+    {
+      id: '26',
+      name: 'Z',
+      type: "b",
+      src: require('../assets/carthemed/zoom.jpg'),
+      objectName: 'Zoom',
+    },
+  ];
+  
+  export default Alphabets;
